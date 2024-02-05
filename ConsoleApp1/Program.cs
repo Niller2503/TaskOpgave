25b812b42a6a9c95110493069e46fc77b08dc7ea
Niller2503 <niklaspetersen441@gmail.com>
2024-02-05 15:20:55 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Diagnostics.Metrics;
+
+Console.WriteLine("App Start");
+
+Task t1 = Task.Factory.StartNew(() => SmartMetode(1, 8000, 1));
+Task t2 = Task.Factory.StartNew(() => SmartMetode(2, 5000, 1));
+Task t3 = Task.Factory.StartNew(() => SmartMetode(3, 2000, 1));
+Task t4 = Task.Factory.StartNew(() => SmartMetode(4, 1100, 1));
+Task t5 = Task.Factory.StartNew(() => SmartMetode(5, 1000, 1));
+
+//Task.WaitAll();
+
+t3.Wait();
+
+Console.WriteLine("App end");
+//Console.ReadLine();
+void SmartMetode(int id, int sleep, int counter)
+{
+    for (int i = 0; i < counter; i++)
+    {
+        Thread.Sleep(sleep);
+        Console.WriteLine($"Task med id: {id} er færdig");
+    }
+}
+
+
+
+
---
+++ Ny App/Ny App.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+/*
+Task<int>.Run(() => MinMetode(50)).ContinueWith(t => { Console.WriteLine(t.Result); });
+Task<int>.Run(() => MinMetode(40)).ContinueWith(t => { Console.WriteLine(t.Result); });
+Task<int>.Run(() => MinMetode(30)).ContinueWith(t => { Console.WriteLine(t.Result); });
+Task<int>.Run(() => MinMetode(20)).ContinueWith(t => { Console.WriteLine(t.Result); });
+Task<int>.Run(() => MinMetode(10)).ContinueWith(t => { Console.WriteLine(t.Result); });
+*/
+Task<int> t1 = Task<int>.Run(() => { return MinMetode(10); });
+Task<int> t2 = Task<int>.Run(() => { return MinMetode(20); });
+Task<int> t3 = Task<int>.Run(() => { return MinMetode(30); });
+Task<int> t4 = Task<int>.Run(() => { return MinMetode(40); });
+Task<int> t5 = Task<int>.Run(() => { return MinMetode(50); });
+
+
+
+
+Task.WaitAll(t1,t2,t3,t4,t5);
+//Console.WriteLine("Done");
+Console.ReadLine();
+
+int MinMetode(int ta)
+{
+    int sum = 0;
+    for (int i = 0; i <= ta; i++)
+    {
+        sum += i;
+
+    }
+    Thread.Sleep(3000);
+    return sum;
+}
